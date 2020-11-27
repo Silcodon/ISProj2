2c1ad2810d89dfd2038ee47e0dd3f9dafaadfb14
Antonio Maria <45206509+Silcodon@users.noreply.github.com>
2020-11-27 13:12:46 +0000

    Client done

--- Projeto2Client/appClientModule/Main.java
+++ Projeto2Client/appClientModule/Main.java
@@ -1,6 +1,11 @@
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,17 +19,22 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import org.junit.Test;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import javax.xml.soap.*;
-
+import org.w3c.dom.Node;
 import common.Publication;
 import common.Researcher;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
+		PrintStream oldErr = System.err;
+		PrintStream newErr = new PrintStream(new ByteArrayOutputStream());
+		System.setErr(newErr);
 		boolean done  = false;
 		while(!done) {
 			System.out.println("****************Web Services****************");
@@ -40,7 +50,44 @@ public class Main {
 			
 			//RESEARCHERS
 			if(option==0) {
-				
+				boolean done_researcher  = false;
+				while(!done_researcher) {
+					String soapEndpointUrl = "http://localhost:8080/ResearcherBeanWS/ResearcherWebService";
+			        String soapAction = "";
+					System.out.println("****************SOAP Researcher WS****************");
+					System.out.println("Pesquisa um researcher por: ");
+					System.out.println("(0) Todas ");
+					System.out.println("(1) Nome ");
+					System.out.println("(2) Skill ");
+					System.out.println("(3) Back ");
+					System.out.println("\n");
+
+					int option_researcher = lerInt(0, 4);
+					//TODAS AS PUBS
+					if(option_researcher==0) {
+				        String method = "Getall";
+				        String param = "";
+				        callSoapWebService(soapEndpointUrl, soapAction, method,param);
+					}
+					//PUBS POR NOME
+					if(option_researcher==1) {
+						System.out.print("Introduza o nome do researcher: ");
+						String param = scan.nextLine();
+						String method = "GetResearcherByNome";
+				        callSoapWebService(soapEndpointUrl, soapAction, method,param);
+					}
+					//PUBS POR RESEARCHER
+					if(option_researcher==2) {
+						System.out.print("Introduza o skill: ");
+						String param = scan.nextLine();
+						String method = "GetResearcherBySkill";
+				        callSoapWebService(soapEndpointUrl, soapAction, method,param);
+					}
+					//SAIR
+					if(option_researcher==3) {
+						done_researcher=true;
+					}
+				}
 			}
 			
 			
@@ -50,12 +97,12 @@ public class Main {
 				while(!done_pub) {
 					String soapEndpointUrl = "http://localhost:8080/PublicationBeanWS/PublicationWebService";
 			        String soapAction = "";
-					System.out.println("****************SOAP WS****************");
+					System.out.println("****************SOAP Publications WS****************");
 					System.out.println("Pesquisa uma publicacao por: ");
 					System.out.println("(0) Todas ");
 					System.out.println("(1) Nome ");
 					System.out.println("(2) Researcher ");
-					System.out.println("(3) Exit ");
+					System.out.println("(3) Back ");
 					System.out.println("\n");
 
 					int option_pub = lerInt(0, 4);
@@ -94,12 +141,12 @@ public class Main {
 			if(option==2) {
 				boolean done_inst  = false;
 				while(!done_inst) {
-					System.out.println("****************REST WS****************");
+					System.out.println("****************REST Institutions WS****************");
 					System.out.println("Pesquisa uma instituição por: ");
 					System.out.println("(0) Todas ");
 					System.out.println("(1) Nome ");
 					System.out.println("(2) Researcher ");
-					System.out.println("(3) Exit ");
+					System.out.println("(3) Back ");
 					System.out.println("\n");
 
 					int option_inst = lerInt(0, 4);
@@ -138,7 +185,7 @@ public class Main {
 			
 		}
 		
-		
+		System.setErr(oldErr);
 		
 	}
 
@@ -286,6 +333,8 @@ public class Main {
             soapBodyElem1.addTextNode(param);
         }
     }
+	
+	
 
     private static void callSoapWebService(String soapEndpointUrl, String soapAction, String method, String param) {
         try {
@@ -295,11 +344,15 @@ public class Main {
 
             // Send SOAP Message to SOAP Server
             SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(soapAction,method,param), soapEndpointUrl);
-            // Print the SOAP Response
-            System.out.println("Response SOAP Message:");
-            soapResponse.writeTo(System.out);
-            System.out.println();
-
+            
+            if(soapEndpointUrl.equals("http://localhost:8080/ResearcherBeanWS/ResearcherWebService")) {
+            	System.out.println();
+                parseSOAPMessageResearcher(soapResponse);
+            }
+            else if(soapEndpointUrl.equals("http://localhost:8080/PublicationBeanWS/PublicationWebService")) {
+            	System.out.println();
+            	parseSOAPMessagePublication(soapResponse);
+            }
             soapConnection.close();
         } catch (Exception e) {
             System.err.println("\nError occurred while sending SOAP Request to Server!\nMake sure you have the correct endpoint URL and SOAPAction!\n");
@@ -307,6 +360,8 @@ public class Main {
         }
     }
 
+    
+    
     private static SOAPMessage createSOAPRequest(String soapAction,String method,String param) throws Exception {
         MessageFactory messageFactory = MessageFactory.newInstance();
         SOAPMessage soapMessage = messageFactory.createMessage();
@@ -318,13 +373,124 @@ public class Main {
 
         soapMessage.saveChanges();
 
-        /* Print the request message, just for debugging purposes */
-        System.out.println("Request SOAP Message:");
-        soapMessage.writeTo(System.out);
-        System.out.println("\n");
 
         return soapMessage;
     }
-	
+    
+    
+    
+    private static void parseSOAPMessageResearcher(SOAPMessage message) throws SOAPException {
+    	SOAPBody soapBody = message.getSOAPBody();
+    	NodeList nodesperson;
+        String someMsgContent = null;
+        String PersonName = null;
+        String Citations = null;
+        String Id = null;
+        String Reads = null;
+        String InstName = null;
+        String InstLocation = null;
+        String InstDepartment = null;
+        ArrayList<String> skillsString = new ArrayList<String>();
+        int k;
+    	// find your node based on tag name
+        NodeList nodesreturn = soapBody.getElementsByTagName("return");
+        // check if the node exists and get the value
+        for(int i=0;i<nodesreturn.getLength();i++) {
+	        nodesperson = nodesreturn.item(i).getChildNodes();
+	        System.out.println("Pessoa " + (i + 1));
+	        k=0;
+	        for(int j=0;j<nodesperson.getLength();j++) {
+	        	if(nodesperson.item(j).getNodeName().equals("citations")) {
+		        	someMsgContent = nodesperson != null ? nodesperson.item(j).getTextContent() : "";
+			        Citations= someMsgContent;
+	        	}
+	        	if(nodesperson.item(j).getNodeName().equals("id")) {
+		        	someMsgContent = nodesperson != null ? nodesperson.item(j).getTextContent() : "";
+			        Id = someMsgContent;
+	        	}
+	        	if(nodesperson.item(j).getNodeName().equals("institutionSet")) {
+	        		NodeList insts = nodesperson.item(j).getChildNodes();
+	        		for(int index=0;index<insts.getLength();index++) {
+	        			if(insts.item(index).getNodeName().equals("department")){
+	        				someMsgContent = insts != null ? insts.item(index).getTextContent() : "";
+	        				InstDepartment=someMsgContent;
+	        			}
+	        			if(insts.item(index).getNodeName().equals("location")){
+	        				someMsgContent = insts != null ? insts.item(index).getTextContent() : "";
+	        				InstLocation=someMsgContent;
+	        			}
+	        			if(insts.item(index).getNodeName().equals("name")){
+	        				someMsgContent = insts != null ? insts.item(index).getTextContent() : "";
+	        				InstName=someMsgContent;
+	        			}
+	        		}
+	        	}
+	        	if(nodesperson.item(j).getNodeName().equals("personName")) {
+		        	someMsgContent = nodesperson != null ? nodesperson.item(j).getTextContent() : "";
+			        PersonName= someMsgContent;
+	        	}
+	        	if(nodesperson.item(j).getNodeName().equals("reads")) {
+		        	someMsgContent = nodesperson != null ? nodesperson.item(j).getTextContent() : "";
+			        Reads = someMsgContent;
+	        	}
+	        	if(nodesperson.item(j).getNodeName().equals("skillSet")) {
+	        		NodeList skills = nodesperson.item(j).getChildNodes();
+	        		someMsgContent = skills != null ? skills.item(0).getTextContent() : "";
+	    		    skillsString.add(someMsgContent);
+	        	}
+	        	
+	        }
+    		System.out.println("Id: " + Id);
+    		System.out.println("PersonName: " + PersonName);
+    		System.out.println("Citations: " + Citations);
+    		System.out.println("Reads: " + Reads);
+			for(k=0; k<skillsString.size();k++) {
+				System.out.println("Skill " + k + ": " + skillsString.get(k));
+			}
+			System.out.println();
+    		System.out.println("Institution ");
+			System.out.println("Name: " + InstName);
+			System.out.println("Location: " + InstLocation);
+			System.out.println("Department: " + InstDepartment);
+	        System.out.println("\n\n");
+	        skillsString.clear();
+	        
+	        
+	        
+        }
+    }
+    
+    
+    
+    private static void parseSOAPMessagePublication(SOAPMessage message) throws SOAPException {
+    	SOAPBody soapBody = message.getSOAPBody();
+    	NodeList nodespub;
+        String someMsgContent = null;
+    	NodeList nodesreturn = soapBody.getElementsByTagName("return");
+        // check if the node exists and get the value
+        for(int i=0;i<nodesreturn.getLength();i++) {
+ 	       nodespub = nodesreturn.item(i).getChildNodes();
+ 	       System.out.println("Publicacao " + (i + 1));
+ 	       for(int j=0;j<nodespub.getLength();j++) {
+ 	       		if(nodespub.item(j).getNodeName().equals("date")) {
+ 	       			System.out.print("Date: ");
+ 	       			someMsgContent = nodespub != null ? nodespub.item(j).getTextContent() : "";
+ 			        System.out.println(someMsgContent);
+ 	        	}
+ 	        	if(nodespub.item(j).getNodeName().equals("name")) {
+ 	       			System.out.print("Name: ");
+ 		        	someMsgContent = nodespub != null ? nodespub.item(j).getTextContent() : "";
+ 			        System.out.println(someMsgContent);
+ 	        	}
+ 	        	if(nodespub.item(j).getNodeName().equals("type")) {
+ 	       			System.out.print("Type: ");
+ 		        	someMsgContent = nodespub != null ? nodespub.item(j).getTextContent() : "";
+ 			        System.out.println(someMsgContent);
+ 	        	}
+ 	        	
+ 	        }
+	        System.out.println("\n\n");
+         }
+    }
 
 }
